a4eb1d8e787ff3b6efc769f7b033f442eb49ca5a
JoanneYang <JoanneYang2004@yahoo.ca>
2022-12-01 08:48:30 -0500

    Refactor

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greet(greeting);
+    res.end(greet(greeting));
 });
 
 const port = 3000;
